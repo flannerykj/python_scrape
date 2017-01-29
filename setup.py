a53bbfc3c3740b81dea93be38e39f10cc0cb6070
flannj <flannj@gmail.com>
2017-01-29 11:50:05 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='scape_chowders',
+      version='1.0',
+      description='Scraping chowders ingredient lists from various recipe site',
+      author='Flannery Jefferson',
+      author_email='flannj@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['distutils', 'distutils.command'],
+     )
